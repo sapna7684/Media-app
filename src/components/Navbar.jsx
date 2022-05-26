7d6160ff54e6f45092a4719b38a8f38497e70c5a
sapna_sharma <sapna7864@gmail.com>
2022-05-26 17:48:44 +0530

    Update text

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,8 @@ export const Navbar = ({searchTerm, setSearchTerm, user}) => {
             <Link to={`/user-profile/${user?._id}`}
                 className="hidden md:block"
             >
-            <img src={user.image} alt="user" className='w-14 h-12 rounded-lg' />
+            {/* <img src={user.image} alt="user" className='w-14 h-12 rounded-lg' /> */}
+            ShareIdea
             </Link>
             <Link to={`/create-pin`}
                 className="bg-black text-white rounded-lg w-12 h-12 md:w-14 md:h-12 flex justify-center items-center"
